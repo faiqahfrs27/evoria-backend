14c0b98c5cb5ae54fbdceb9205b9c0fbc7399808
Faiqah Farras F <faiqah.farrasf@gmail.com>
2026-04-15 22:48:29 +0700

    Feature/register (#15)
    * feat: register
    
    * refactor(register): improve email normalization and referral validation logic
    
    * feat: register
    
    * fix: change logic in register
    
    * fix: typo on file schema prisma and app.ts
    
    * fix: reset prisma migrations and sync schema (uuid + role + relations) (#10)
    
    * feat: add validationMiddleware and dto (#12)
    
    * feat: add logic for referral code, coupon, and point

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -1,3 +1,4 @@
 export const EXPIRED_ACCESS_TOKEN_JWT = "15m";
 export const EXPIRED_REFRESH_TOKEN_JWT = "7d";
 export const EXPIRED_7_DAY = new Date(Date.now() + 7 * 24 * 60 * 1000);
+export const REFERRAL_EXPIRED_MONTH = 3;
