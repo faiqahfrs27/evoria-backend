86c830a6b34861d7d9e8abcb5e3c40e753a92a23
Grace-dotcom123 <grace280404@gmail.com>
2026-04-17 08:16:54 +0700

    feat: get-events

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,12 +1,32 @@
 import { plainToInstance } from "class-transformer";
 import { validate } from "class-validator";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { ApiError } from "../../utils/api-error.js";
 import { EventService } from "./event.service.js";
 import { CreateEventDTO } from "./dto/create-event.dto.js";
 
 export class EventController {
   constructor(private eventService: EventService) {}
+
+  getEvents = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const query = req.query;
+      const result = await this.eventService.getEvent(query as any);
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getEventById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.id as string;
+      const event = await this.eventService.getEventById(id);
+      res.status(200).json({ data: event });
+    } catch (error) {
+      next(error);
+    }
+  };
 
   createEvent = async (req: Request, res: Response) => {
     const body = plainToInstance(CreateEventDTO, req.body);
